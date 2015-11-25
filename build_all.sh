447d0c9c3b44547a8dad9ebdcbc4daeafc86db53
Alexander Larsson <alexl@redhat.com>
2015-11-25 15:33:19 +0100

    build_all.sh: convert to xdg-app-build

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-for i in *.def; do
-    ./build.sh $i
+for i in *.json; do
+    APPID=`basename $i .json`
+    echo ========== Building $APPID ================
+    if xdg-app-builder --require-changes app $i; then
+        if test -d app; then
+            echo ========== Exporting $APPID ================
+            xdg-app build-export --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} repo app
+            if [ "x${GPG_SIGN_KEY}" != "x" ]; then
+                echo ostree gpg-sign --repo=repo app/$APPID/x86_64/master ${GPG_SIGN_KEY}
+            fi
+        fi
+    fi
 done
