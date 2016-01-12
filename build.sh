b74e14d1d9b51cb52d4939a60041ed3a5a0f0b66
Alexander Larsson <alexl@redhat.com>
2016-01-12 14:42:26 +0100

    build.sh: Use latest features of xdg-app-builder

--- build.sh
+++ build.sh
@@ -5,12 +5,4 @@ FILE=$1
 APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
-if xdg-app-builder --require-changes app $FILE; then
-    if test -d app; then
-        echo ========== Exporting $APPID ================
-        xdg-app build-export --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} repo app
-        if [ "x${GPG_SIGN_KEY}" != "x" ]; then
-            echo ostree gpg-sign --repo=repo app/$APPID/x86_64/master ${GPG_SIGN_KEY}
-        fi
-    fi
-fi
+xdg-app-builder --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
