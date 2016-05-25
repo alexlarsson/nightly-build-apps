4ea435e07ba5464609c0926d2f9802cfda73d7f0
Alexander Larsson <alexl@redhat.com>
2016-05-25 11:12:58 +0200

    Update to flatpak

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ FILE=$1
 APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
-rm -rf app
-xdg-app-builder --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
+flatpak-builder --force-clean --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
--- build_update_repo.sh
+++ build_update_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xdg-app build-update-repo --generate-static-deltas --prune --prune-depth=8  ${EXPORT_ARGS-} repo
+flatpak build-update-repo --generate-static-deltas --prune --prune-depth=8  ${EXPORT_ARGS-} repo
