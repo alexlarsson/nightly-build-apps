c102d59c12f01c3ad73aa169d73c1d21d683c552
Alexander Larsson <alexl@redhat.com>
2017-03-20 15:32:46 +0100

    Revert accidental change of gpg args

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ FILE=$1
 APPID=`basename $FILE .json`
 
 echo ========== Building $APPID ================
-flatpak-builder --force-clean --ccache --require-changes --gpg-sign=44F17A57 --gpg-homedir=/home/jgrulich/.gnupg/ --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
+flatpak-builder --force-clean --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
